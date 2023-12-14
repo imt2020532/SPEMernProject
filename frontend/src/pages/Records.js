8ae429ba42e2f90f149c6dd608f4524229fa65b0
imt2020532 <anwit.damale@iiitb.org>
2023-12-14 10:59:41 +0530

    features

---
+++ frontend/src/pages/Records.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Records = () => {
+  return (
+    <div className="records">
+      <h1>Records Page</h1>
+      {/* Add your content for the Records page */}
+    </div>
+  );
+};
+
+export default Records;
